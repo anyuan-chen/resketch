a4165de67456789aa2b1afdb86ba29a3e23347f3
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-04-08 18:09:50 -0400

    home page

--- frontend/hooks/useModal.js
+++ frontend/hooks/useModal.js
@@ -1,19 +1,25 @@
 import React from "react";
 
 const useModal = () => {
-    const [isShowing, setIsShowing] = React.useState(false);
-    const [text, setText] = React.useState("");
-    function toggle() {
-        setIsShowing(!isShowing);
-    }
-    function changeText(value) {
-        setText(value);
-    }
-    return {
-        isShowing,
-        toggle,
-        changeText,
-        text
-    }
-}
+  const [isShowing, setIsShowing] = React.useState(false);
+  const [text, setText] = React.useState("");
+  const [name, setName] = React.useState("");
+  function toggle() {
+    setIsShowing(!isShowing);
+  }
+  function changeText(value) {
+    setText(value);
+  }
+  function changeName(value) {
+    setName(value);
+  }
+  return {
+    isShowing,
+    toggle,
+    changeText,
+    text,
+    changeName,
+    name,
+  };
+};
 export default useModal;
