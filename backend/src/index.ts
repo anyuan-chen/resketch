b07b916015d712bdc85e6c0d857bcaa1bc7255ad
eggy <eggyrules@gmail.com>
2022-04-08 18:09:40 -0400

    Add basic stuff

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,16 @@
-import { WebSocketServer } from "ws";
+import { WebSocket, WebSocketServer } from "ws";
+
+const server = new WebSocketServer();
+
+let sockets: WebSocket[] = [];
+server.on("connection", (socket) => {
+  sockets.push(socket);
+
+  // on receive
+  socket.on("message", (msg) => {});
+
+  // remove socket on disconnect
+  socket.on("close", () => {
+    sockets = sockets.filter((s) => s !== socket);
+  });
+});
