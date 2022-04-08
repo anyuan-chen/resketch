9a28c4021c601e6b1ec9331dac6a5b6b71555c25
Daniel Chen <25178974+potatoeggy@users.noreply.github.com>
2022-04-08 18:16:26 -0400

    Init ws backend (#4)
    * Add basic stuff
    
    * Set up server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,20 @@
-import { WebSocketServer } from "ws";
+import { WebSocket, WebSocketServer } from "ws";
+import globalState from "./state";
+
+const server = new WebSocketServer({ port: 8080 });
+
+let sockets: WebSocket[] = [];
+server.on("connection", (socket) => {
+  sockets.push(socket);
+
+  // on receive
+  socket.on("message", (msg) => {});
+
+  // remove socket on disconnect
+  socket.on("close", () => {
+    sockets = sockets.filter((s) => s !== socket);
+  });
+});
+
+console.log("WebSocket server started");
+console.log("Probably at ws://localhost:8080 for dev");
