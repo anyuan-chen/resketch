fa09c124327f9db108fd8e4577e04db9ab33c308
eggy <eggyrules@gmail.com>
2022-04-09 11:09:34 -0400

    frontend: docs

--- craFrontend/my-app/src/routes/game.js
+++ craFrontend/my-app/src/routes/game.js
@@ -3,5 +3,16 @@ import { SocketAPI } from "../utils/socketapi";
 
 export default function Game() {
   const server = new SocketAPI();
+  // watch the following for server changes:
+  /*
+   * server.users
+   * server.guild_id
+   * server.currentRound
+   * server.totalRounds
+   * server.victories
+   * server.images
+   * server.confidences
+   * server.currentWord
+   */
   return <div>Game</div>;
 }
