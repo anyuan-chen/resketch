87b02b345b9ad29f60235e7f48813cfbd6a35399
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-04-09 09:56:31 -0400

    websocket code

--- craFrontend/my-app/src/routes/game.js
+++ craFrontend/my-app/src/routes/game.js
@@ -3,9 +3,17 @@ import React from "react";
 export default function Game() {
   const url = new URL(window.location.href);
   const socket = new WebSocket(
-    `ws://localhost:8080?url=${url.searchParams.get(
+    `ws://localhost:8080/${url.pathname}?guild=${url.searchParams.get(
       "guild"
-    )}&rounds=${url.searchParams.get("3")}`
+    )}&rounds=${url.searchParams.get("rounds")}`
+  );
+
+
+  
+  console.log(
+    `ws://localhost:8080${url.pathname}?guild=${url.searchParams.get(
+      "guild"
+    )}&rounds=${url.searchParams.get("rounds")}`
   );
   socket.addEventListener("open", function (event) {
     socket.send("Hello Server!");
