7812e7db1f2d5dc7bb4c4de094b6e096e9c15f29
eggy <eggyrules@gmail.com>
2022-04-09 10:36:00 -0400

    Make stuff happen

--- craFrontend/my-app/src/routes/game.js
+++ craFrontend/my-app/src/routes/game.js
@@ -1,25 +1,7 @@
 import React from "react";
+import { SocketAPI } from "../utils/socketapi";
 
 export default function Game() {
-  const url = new URL(window.location.href);
-  const socket = new WebSocket(
-    `ws://localhost:8080/${url.pathname}?guild=${url.searchParams.get(
-      "guild"
-    )}&rounds=${url.searchParams.get("rounds")}`
-  );
-
-
-  
-  console.log(
-    `ws://localhost:8080${url.pathname}?guild=${url.searchParams.get(
-      "guild"
-    )}&rounds=${url.searchParams.get("rounds")}`
-  );
-  socket.addEventListener("open", function (event) {
-    socket.send("Hello Server!");
-  });
-  socket.addEventListener("message", function (event) {
-    console.log("Message from server ", event.data);
-  });
+  const server = new SocketAPI();
   return <div>Game</div>;
 }
