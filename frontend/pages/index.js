da20bd1da4ef34f22b9dbd0200c62737fbb1026b
magicalsoup <amagicalsoup@gmail.com>
2022-04-08 21:46:52 -0400

    finish lobby component and center text in button by fixing the button component

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
+import Lobby from "../components/lobby";
 
 export default function Home() {
-  return <div></div>;
+  return <div> <Lobby/> </div>;
 }
